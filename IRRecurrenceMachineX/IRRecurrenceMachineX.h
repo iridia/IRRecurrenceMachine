b75187dc0f42b4e483855899bd13deb86a023192
Evadne Wu <ev@monoceroi.com>
2011-12-18 00:21:49 +0800

    OS X target

---
+++ IRRecurrenceMachineX/IRRecurrenceMachineX.h
@@ -0,0 +1,10 @@
+//
+//  IRRecurrenceMachineX.h
+//  IRRecurrenceMachineX
+//
+//  Created by Evadne Wu on 12/18/11.
+//  Copyright (c) 2011 Iridia Productions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "IRRecurrenceMachine.h"
---
+++ IRRecurrenceMachineX/IRRecurrenceMachineX.m
@@ -0,0 +1,9 @@
+//
+//  IRRecurrenceMachineX.m
+//  IRRecurrenceMachineX
+//
+//  Created by Evadne Wu on 12/18/11.
+//  Copyright (c) 2011 Iridia Productions. All rights reserved.
+//
+
+#import "IRRecurrenceMachineX.h"
